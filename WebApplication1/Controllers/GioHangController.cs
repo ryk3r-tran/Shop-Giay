b4096d64e43ade5aff0215c6bfb426374516ef9f
R Y K 3 R <honeyhieutai@gmail.com>
2022-10-06 15:03:13 +0700

    update giohang, donhang, taikhoan

---
+++ WebApplication1/Controllers/GioHangController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GioHang()
+        {
+            return View();
+        }
+
+        public ActionResult ThanhToan()
+        {
+            return View();
+        }
+
+        public ActionResult XacNhanDonHang()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/KiemTraDonHangController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class KiemTraDonHangController : Controller
+    {
+        // GET: KiemTraDonHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult KiemTraDonHang()
+        {
+            return View();
+        }
+    }
+}
