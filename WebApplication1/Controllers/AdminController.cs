8f6439d531315f9762381664267dec7a73d4dc31
R Y K 3 R <honeyhieutai@gmail.com>
2022-10-06 23:25:03 +0700

    big update admin

---
+++ WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DieuKhien()
+        {
+            return View();
+        }
+        public ActionResult TatCaTaiKhoan()
+        {
+            return View();
+        }
+
+        public ActionResult ThemTaiKhoan()
+        {
+            return View();
+        }
+
+        public ActionResult ThongKe()
+        {
+            return View();
+        }
+
+        public ActionResult TatCaSanPham()
+        {
+            return View();
+        }
+
+        public ActionResult ThemSanPham()
+        {
+            return View();
+        }
+    }
+}
