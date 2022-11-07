572cec1bc1d5ed395c39dea55d778b6b97dc354e
R Y K 3 R <honeyhieutai@gmail.com>
2022-11-07 15:44:52 +0700

    test Tai

--- WebApplication1/Controllers/SanPhamController.cs
+++ WebApplication1/Controllers/SanPhamController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KarmaModels.KarmaModels;
+using KarmaModels.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,20 @@ namespace WebApplication1.Controllers
 {
     public class SanPhamController : Controller
     {
+        KarmaDBContext _context = new KarmaDBContext();
         // GET: SanPham
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult DanhSachSanPham()
         {
-            return View();
+            IRepository<SANPHAM> sanpham = new Repository<SANPHAM>();
+            var data = sanpham.GetAll();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Find(string search_input)
+        {
+            var search_name = search_input.ToLower();
+            var listProd = _context.SANPHAMs.Where(s => s.TenSP.ToLower().Contains(search_name)).ToList();
+            return RedirectToAction("DanhSachSanPham", "SanPham",listProd);
         }
 
         public ActionResult SanPhamChiTiet()
