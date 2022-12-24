c7c8e9a3fc6d0485b340af08ca1a41a20f985e35
manhpham123 <Manh21042001@gmail.com>
2022-12-24 22:27:41 +0700

    update thanh toan

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -4,11 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using KarmaModels.KarmaModels;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class TestController : Controller
     {
-       
+       public ActionResult Test()
+        {
+
+            List<TinhThanh> listTinh = new List<TinhThanh>();
+            TinhThanh ttp;
+            ttp = new TinhThanh();
+            ttp.Ma = 1;
+            ttp.Ten = "ha noi";
+            listTinh.Add(ttp);
+
+            ttp = new TinhThanh();
+            ttp.Ma = 2;
+            ttp.Ten = "hcm";
+            listTinh.Add(ttp);
+
+            ttp = new TinhThanh();
+            ttp.Ma = 3;
+            ttp.Ten = "da nang";
+            listTinh.Add(ttp);
+
+            ViewBag.TinhThanh = new SelectList(listTinh, "Ma", "Ten");
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Test(int gioitinh)
+        {
+
+            return View();
+        }
     }
 }
